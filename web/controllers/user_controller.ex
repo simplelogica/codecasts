fc379d1322594ab2bfbd42ac3b652dd4ebce6288
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-06-29 16:44:29 +0200

    Add password hashing.

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -9,12 +9,12 @@ defmodule Codecasts.UserController do
   end
 
   def new(conn, _params) do
-    changeset = User.changeset(%User{})
+    changeset = User.registration_changeset(%User{})
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"user" => user_params}) do
-    changeset = User.changeset(%User{}, user_params)
+    changeset = User.registration_changeset(%User{}, user_params)
 
     case Repo.insert(changeset) do
       {:ok, _user} ->
--- web/models/user.ex
+++ web/models/user.ex
@@ -13,13 +13,39 @@ defmodule Codecasts.User do
   end
 
   @doc """
-  Builds a changeset based on the `struct` and `params`.
+  Builds a changeset based on the `model` and `params`.
   """
-  def changeset(struct, params \\ :empty) do
-    struct
+  def changeset(model, params \\ %{}) do
+    model
     |> cast(params, [:username, :name, :email], [:bio])
     |> validate_required([:username, :name, :email])
     |> unique_constraint(:username)
     |> unique_constraint(:email)
+  end
+
+  @doc """
+  Registration changeset that uses the normal changeset and also requires a password to generate
+  a valid password hash.
+  """
+  def registration_changeset(model, params \\ %{}) do
+    model
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> generate_password_hash()
+  end
+
+  # Auxiliar function to generate the password hash and put it on the changeset
+  #
+  defp generate_password_hash(changeset) do
+    case changeset do
+      # If it is a valid changeset and contains a password, regenerate the hash
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Comeonin.Bcrypt.hashpwsalt(pass))
+
+      # If not, just return the changeset without changes
+      _ ->
+        changeset
+    end
   end
 end
