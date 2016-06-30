14d4e173da22e1306d0ad6a356c6cb92f8531731
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-06-30 12:34:02 +0200

    Add Guardian, it's config and a GuardianSerializer.

--- mix.exs
+++ mix.exs
@@ -39,7 +39,8 @@ defmodule Codecasts.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:comeonin, "~> 2.0"}
+      {:comeonin, "~> 2.0"},
+      {:guardian, "~> 0.12.0"}
    ]
   end
 
