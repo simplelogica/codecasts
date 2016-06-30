47d73b32569a623e913fc01e2e784fa975ce11e0
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-06-30 12:22:06 +0200

    Use custom helpers to show bootstrap flash alerts.

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -1,3 +1,21 @@
 defmodule Codecasts.LayoutView do
   use Codecasts.Web, :view
+  import Phoenix.Controller, only: [get_flash: 1]
+
+  # Function to show flash messages using bootstrap classes
+  def show_flash(conn) do
+    get_flash(conn) |> flash_msg
+  end
+
+  def flash_msg(%{"info" => msg}) do
+    ~E"<div class='alert alert-info'><%= msg %></div>"
+  end
+
+  def flash_msg(%{"error" => msg}) do
+    ~E"<div class='alert alert-danger'><%= msg %></div>"
+  end
+
+  def flash_msg(_) do
+    nil
+  end
 end
