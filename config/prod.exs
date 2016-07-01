5337073825ba83c015176834860a648827e19214
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-07-01 14:08:37 +0200

    Update prod config.

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :codecasts, Codecasts.Endpoint,
 config :logger, level: :info
 
 
-config :codecasts, Codecasts.Endpoint,
+config :codecasts, Codecasts.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
