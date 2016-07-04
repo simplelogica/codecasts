ac189d649f580c93a8cdbcc792d1b92463fc4550
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-07-04 21:14:21 +0200

    Change landing page to show next/last events.

--- web/models/event.ex
+++ web/models/event.ex
@@ -27,6 +27,34 @@ defmodule Codecasts.Event do
     |> validate_format_optional(:repository_url, ~r/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/)
   end
 
+  @doc """
+  Scope to get `number` events coming soon.
+  """
+  def next_events(number \\ 3) do
+    actual_date = Ecto.DateTime.from_erl(:calendar.universal_time())
+
+    from e in __MODULE__,
+      where: e.date >= ^actual_date,
+      limit: ^number,
+      order_by: [asc: e.date],
+      select: e
+  end
+
+  @doc """
+  Scope to get `number` events past.
+  """
+  def last_events(number \\ 3) do
+    actual_date = Ecto.DateTime.from_erl(:calendar.universal_time())
+
+    from e in __MODULE__,
+      where: e.date <= ^actual_date,
+      limit: ^number,
+      order_by: [desc: e.date],
+      select: e
+  end
+
+  # Custom validation to perform format validation only if the field has value.
+  #
   defp validate_format_optional(changeset, field, format, opts \\ []) do
     if (changeset.changes[field] && String.length(changeset.changes[field]) > 0) do
       changeset
