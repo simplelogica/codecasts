646f06015f16c6b77a16d7d7ef83066ef39f05a7
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-07-04 15:20:29 +0200

    Modify event form to add place field.

--- web/views/event_view.ex
+++ web/views/event_view.ex
@@ -1,3 +1,14 @@
 defmodule Codecasts.EventView do
   use Codecasts.Web, :view
+
+  @event_place_names %{
+    madrid: "Madrid",
+    oviedo: "Oviedo",
+    online: "Online"
+  }
+
+  def get_event_place_name(event_place) do
+    @event_place_names
+    |> Map.get(event_place)
+  end
 end
